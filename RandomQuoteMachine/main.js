ce3c067e81bf81197d28f0c97eaf2e26a7f2c470
franromeroe <franromeroeze@gmail.com>
2023-06-12 18:54:58 -0300

    definicion de props

--- RandomQuoteMachine/main.js
+++ RandomQuoteMachine/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
 
 //componente QuoteBox <QuoteBox/>
 //estructura de elementos html
-const QuoteBox = () => (
+const QuoteBox = ({ quote, handleNewQuote }) => (
 	<div id='quote-box'>
     <p id='text'>{quote.text}</p>
     <h2 id='author'>{quote.author}</h2>
@@ -26,10 +26,16 @@ const QuoteBox = () => (
   </div>
 );
 //componente App <App/>
-const App = () => (
-  <div class='main'>
-  	<QuoteBox/>
-  </div>
-);
+const App = () => {
+  const [quote, setQuote] = React.useState({ text: 'Hello world', author: 'Mahammed'})
+  const handleNewQuote = () =>{
+    
+  }
+  return (
+    <div className='main'>
+      <QuoteBox quote={quote} handleNewQuote={handleNewQuote}/>
+    </div>
+  ); 
+};
 //(nombre de la variable , seleccion del elemento html)
-ReactDOM.render(<App/>, document.querySelector('#app'))
+ReactDOM.render(<App />, document.querySelector('#app'))
